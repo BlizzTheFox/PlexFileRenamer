24ce8f89737717c59c5d1adb3082c890408f1595
BlizzTheFox <bartbastiaensen@gmail.com>
2025-09-24 18:41:14 +0200

    v1

---
+++ ReadMe.txt
@@ -0,0 +1,19 @@
+Version 1.0
+
+This program will help to rename a large amount of files (like anime episodes) to follow the recommended naming scheme for Plex Media Servers.
+
+the naming scheme it uses:
+    Anime Or Show Name s#e#
+
+How to use:
+1) Run the executable
+2) Enter a directory path to a folder
+3) Follow instructions in the console
+
+IMPORTANT:
+    - The program will use the name of the final directory as the name for the show/anime
+        - Example: "D:\Media\Anime\Anime Name" it will use "Anime Name" as the name of the show/anime
+    - The program will search this directory for folders or with the naming scheme "Season #"
+        - This is case-sensitive
+        - # can be any whole number (1, 2, 3, 4, 5)
+    - The program will keep the original filetype of any renamed files
--- main.cpp
+++ main.cpp
@@ -3,92 +3,99 @@
 #include <iostream>
 #include <string>
 #include <filesystem>
-#include <fstream>
 #include <vector>
-#include <windows.h>
 
-using namespace std;
-namespace fs = filesystem;
+using namespace std; //bite it's me less typing
+namespace fs = filesystem; //bite me again it's less typing
 
 bool running = true;
-string fileLocation[2] = {("null"), ("null")};
+string fileLocation[2] = {("D:\\Media\\Anime Or Show Name"), ("D:\\Media\\Anime Or Show Name")};
 string animeName;
 
 vector<string> seasonsFolderPaths;
 
 int main() {
+    //Giving some information
+    printColour(aqua, "Welcome to Plex File Renamer!\n");
+    printColour(white,"Info about how the program works is in ReadMe.txt\n\n");
+
     while (running) {
-    //asking user for relevant data (directory, anime name)
-    printColour(white, "the last file location was: "); printColour(green, fileLocation[1]);
-    //Directory
-    printColour(aqua, "\nInput the full directory path"); printColour(white, ": ");
-    getline(cin >> ws, fileLocation[0]);
-    //Anime name
-    printColour(aqua, "What is the name of the show/anime"); printColour(white, ": ");
-    getline(cin >> ws, animeName);
+        //asking user for relevant data (directory)
+        printColour(white, "The last/example file location was: "); printColour(green, fileLocation[1]);
 
-    //Getting all the directories to the seasons folders
-    seasonsFolderPaths = getSeasonsFolders(fileLocation[0]);
+        //Directory
+        printColour(aqua, "\nInput the full directory path"); printColour(white, ": ");
+        getline(cin >> ws, fileLocation[0]);
 
-    //Displaying all the found folders
-    printColour(white, "Folders found: \n");
-    setColour(gray);
-    for (fs::path filePath : seasonsFolderPaths) {
-        cout << filePath << endl;
-    }
-    setColour(white);
+        //Anime name
+        fs::path directory = fileLocation[0];
+        animeName = directory.filename().string();
 
-    for (fs::path filePath : seasonsFolderPaths) {
-        vector<string> filesInSeasonFolder = getFilesInDirectory(filePath);
-        filesInSeasonFolder = sortVector(filesInSeasonFolder);
+        //Getting all the directories to the seasons folders
+        seasonsFolderPaths = getSeasonsFolders(fileLocation[0]);
 
-        string seasonNumber = filePath.filename().string().erase(0, 7); //erase(0,7) will turn "Season #" into "#"
-
-        //displaying the files found in the seasons folder
-        printColour(aqua, "\nFiles found in "); printColour(green, filePath.filename().string()); printColour(white, ": \n");
+        //Displaying all the found folders
+        printColour(aqua, "Folders found"); printColour(white, ": \n");
         setColour(gray);
-        for (fs::path file : filesInSeasonFolder) {
-            cout << file << endl;
+        for (fs::path filePath : seasonsFolderPaths) {
+            cout << filePath.string() << endl;
         }
-        cout << endl << endl; setColour(white);
+        setColour(white);
 
-        //generating new file names
-        vector<string> fileExtention;
-        vector<string> newFileName;
-        int i = 0;
-        for (fs::path file : filesInSeasonFolder) {
-            fileExtention.push_back(file.extension().string());
-            newFileName.push_back(animeName + " s" + seasonNumber + "e" + to_string(i+1));
+        for (fs::path filePath : seasonsFolderPaths) {
+            vector<string> filesInSeasonFolder = getFilesInDirectory(filePath);
+            filesInSeasonFolder = sortVector(filesInSeasonFolder);
 
-            cout << file.string() << "\t-->\t" << file.replace_filename(newFileName[i]).replace_extension(fileExtention[i]).string() << endl;
-            i++;
+            string seasonNumber = filePath.filename().string().erase(0, 7); //erase(0,7) will turn "Season #" into "#"
+
+            //displaying the files found in the seasons folder
+            printColour(aqua, "\nFiles found in "); printColour(green, filePath.filename().string()); printColour(white, ": \n");
+            setColour(gray);
+            for (fs::path file : filesInSeasonFolder) {
+                cout << file.string() << endl;
+            }
+            cout << "\n\n"; setColour(white);
+
+            //generating new file names
+            vector<string> fileExtention;
+            vector<string> newFileName;
+            int i = 0;
+
+            printColour(aqua, "New names for the files in "); printColour(green, filePath.string()); printColour(white, ": \n");
+            for (fs::path file : filesInSeasonFolder) {
+                fileExtention.push_back(file.extension().string());
+                newFileName.push_back(animeName + " s" + seasonNumber + "e" + to_string(i+1));
+
+                //displaying the new names for the files
+                cout << file.filename().string() << "\t\t-->\t\t" << file.replace_filename(newFileName[i]).replace_extension(fileExtention[i]).filename().string() << endl;
+                i++;
+            }
+
+            //Checking if the user wishes to continue renaming the files
+            char continueChoice;
+            printColour(aqua, "Do you wish to continue renaming these files? "); printColour(green, "(y/n)"); printColour(white, ": ");
+            cin >> continueChoice;
+            if (continueChoice != 'y') { break; }
+
+            //Renaming files
+            i = 0;
+            for (fs::path file : filesInSeasonFolder) {
+                fs::path oldFilePath = file;
+                fs::path newFilePath = file.replace_filename(newFileName[i]).replace_extension(fileExtention[i]);
+                rename(oldFilePath, newFilePath);
+                i++;
+            }
         }
-
-        //Checking if the user wishes to continue renaming the files
-        char continueChoice;
-        printColour(aqua, "Do you wish to continue renaming these files? "); printColour(green, "(y/n)"); printColour(white, ": ");
-        cin >> continueChoice;
-        if (continueChoice != 'y') { break; }
-
-        //Renaming files
-        i = 0;
-        for (fs::path file : filesInSeasonFolder) {
-            fs::path oldFilePath = file;
-            fs::path newFilePath = file.replace_filename(newFileName[i]).replace_extension(fileExtention[i]);
-            rename(oldFilePath, newFilePath);
-            i++;
-        }
-    }
-        //checking if the user wants to restart the program
-        char restartProgramChar;
-        printColour(aqua, "Do you wish to restart the program? "); printColour(green, "(y/n)"); printColour(white, ": ");
-        cin >> restartProgramChar;
-        if (restartProgramChar != 'y') {
-            running = false;
-        } else {
-            running = true;
-            fileLocation[1] = fileLocation[0];
-        }
+            //checking if the user wants to restart the program
+            char restartProgramChar;
+            printColour(aqua, "Do you wish to restart the program? "); printColour(green, "(y/n)"); printColour(white, ": ");
+            cin >> restartProgramChar;
+            if (restartProgramChar != 'y') {
+                running = false;
+            } else {
+                running = true;
+                fileLocation[1] = fileLocation[0];
+            }
     }
 
     return 0;
